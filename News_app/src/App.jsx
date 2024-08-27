4dbe16aad2ad922b90eccf93459c85d596544127
Stuti Srivastava <stutisrivastava024@gmail.com>
2024-08-27 07:40:15 +0530

    first commit

---
+++ News_app/src/App.jsx
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './App.css';
+import Newsapp from './components/Newsapp';
+
+
+function App() {
+  return (
+    <Newsapp/>
+  );
+} 
+
+export default App;
